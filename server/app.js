9898a035164351cc8e9220be01a9b6a8df6bc81c
Arnold Rozon <arnold@rozon.org>
2019-11-02 16:29:45 -0500

    Set up single page app

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
-const BUNDLE = path.resolve(__dirname, "../client/build", "index.html");
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const BUNDLE = path.resolve(__dirname, "../client/build", "index.html");
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -13,14 +13,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // set public dir for app
 app.use("/", express.static(path.join(__dirname, "../client", "build")));
 
 // routes
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
 
 // serve app
 app.get("*", (req, res) => res.sendFile(BUNDLE));
