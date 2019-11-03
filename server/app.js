e5a25b58ecdf529e6189c3d0de3bd674e744d9f3
Arnold Rozon <arnold@rozon.org>
2019-11-03 15:55:34 -0600

    Adds sign up route

--- server/app.js
+++ server/app.js
@@ -1,19 +1,18 @@
 const express = require("express");
 const path = require("path");
-const BUNDLE = path.resolve(__dirname, "../client/build", "index.html");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const got = require("got");
-const passportService = require("./services/passport");
-const passport = require("passport");
 
+// Auth deps
+const bcrypt = require("bcrypt");
+const User = require("./models/User");
+const jwt = require("jsonwebtoken");
+
+// Mongo deps
+const mongoose = require('mongoose');
 const MONGO_URI = "mongodb://localhost:27017/invoke-alpha";
-
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
-
-// const apiRoutes = require("./routes/api");
-// const authRoutes = require("./routes/auth");
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 const app = express();
 
@@ -25,7 +24,7 @@ app.use(cookieParser());
 // set public dir for app
 app.use("/", express.static(path.join(__dirname, "../client", "build")));
 
-const CLIENT_SECRET = "k36pJmOOISyOeUsn";
+const LINKEDIN_CLIENT_SECRET = "k36pJmOOISyOeUsn";
 
 app.post("/auth/linkedin", async (req, res) => {
   const { body } = req;
@@ -34,7 +33,7 @@ app.post("/auth/linkedin", async (req, res) => {
     code: body.authorizationCode,
     redirect_uri: "http://localhost:3000/auth",
     client_id: "86n2guu7lpgeen",
-    client_secret: CLIENT_SECRET
+    client_secret: LINKEDIN_CLIENT_SECRET
   };
 
   try {
@@ -94,7 +93,74 @@ app.get("/auth/linkedin/user", async (req, res) => {
   }
 });
 
+app.get("/auth/native/register", async (req, res) => {
+  if (!req.body.password || !req.body.email) {
+    return res.status(400).json({ 
+      message: "Invalid registration credentials provided"
+    });
+  }
+
+  // Check if email in use
+  // If not proceed with...
+  User.findOne({ email: req.body.email })
+    .exec()
+    .then(function(user) {
+      if (user) {
+        return res.status(400).json({
+          message: "A user already exists with that email address"
+        });
+      }
+
+      // Hashing of password
+      // Creating a new User();
+      // Saving them to the db
+      // Returning a JWT
+      bcrypt.hash(req.body.password, 10, function(err, hash) {
+        if (err) {
+          // Denotes bcrypt error
+          return res.status(500).json({
+            message: "Error creating new user"
+          });
+        } else {
+          const newUser = new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: req.body.email,
+            password: hash
+          });
+
+          newUser
+            .save()
+            .then(function(result) {
+              console.log("** New user created: **", result);
+
+              // Issue JWT here
+              res.status(200).json({
+                success: "New user created successfully",
+                token: null
+              });
+            })
+            .catch(error => {
+              // Denotes mongo error
+              return res.status(500).json({
+                message: "Error saving new user"
+              });
+            });
+        }
+      });
+      // End bcrypt
+    })
+    .catch(error => {
+      return res.status(500).json({
+        message: "Error creating new user"
+      });
+    });
+});
+
+app.get("/auth/native/signin", (req, res) => {
+});
+
 // serve app
+const BUNDLE = path.resolve(__dirname, "../client/build", "index.html");
 app.get("*", (req, res) => res.sendFile(BUNDLE));
 
 module.exports = app;
