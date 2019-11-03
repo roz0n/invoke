c50b352112151004693d1a1eeb80d928dce65adf
Arnold Rozon <arnold@rozon.org>
2019-11-02 21:20:52 -0500

    Progress on LinkedIn auth but not there yet

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
 import "./App.css";
-import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+const GET = "GET";
+const POST = "POST";
+const SECRET = "Arnaldo was here";
 
-const authEndpoint = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86n2guu7lpgeen&redirect_uri=http://localhost:3000/auth&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social`;
+const authEndpoint = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86n2guu7lpgeen&redirect_uri=http://localhost:3000/auth&state=${SECRET}&scope=r_liteprofile%20r_emailaddress%20w_member_social`;
+
+const fullPageStyle = {
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  justifyContent: "space-around",
+  height: "100vh",
+  width: "100%"
+};
+
+async function mockRequest(setError, setValue, options = { method: GET }) {
+  switch (options.method) {
+    case "GET":
+      return fetch("https://swapi.co/api/planets/", { ...options })
+        .then(res => res.json())
+        .then(data => setValue(data))
+        .catch(() => setError(true));
+    case "POST":
+      return fetch("/auth/linkedin", { ...options })
+        .then(res => res.json())
+        .then(data => setValue(data))
+        .catch(() => setError(true));
+    default:
+      break;
+  }
+}
 
 function App() {
   return (
@@ -29,19 +64,64 @@ function Home() {
   );
 }
 
-function Auth() {
-  const style = {
-    display: "flex",
-    alignItems: "center",
-    flexDirection: "column",
-    justifyContent: "space-around",
-    height: "100vh",
-    width: "100%"
-  };
+function Auth({ location }) {
+  const { search: searchParams } = location;
+  const [error, setError] = useState(false);
+  const [userData, setUserData] = useState(null);
 
+  const authorizationCode =
+    searchParams.split("code=")[1] &&
+    searchParams.split("code=")[1].split("&")[0];
+
+  useEffect(() => {
+    if (authorizationCode) {
+      const reqBody = {
+        authorizationCode,
+        secret: SECRET
+      };
+
+      mockRequest(setError, setUserData, {
+        method: POST,
+        body: JSON.stringify(reqBody),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+    } else {
+      setError(true);
+    }
+  }, []);
+
+  // TODO:
+  // We must extract url params here and post with the access token [DONE]
+  // If successful, redirect somewhere can can store the necessary tokens
+  // Maybe a redirect page, or a user profile page, or maybe the former first then the latter
+  // If unsuccessful, redirect somewhere back home and present an error
+
+  console.log("authorization_code:", authorizationCode);
   return (
-    <div className="Auth" style={style}>
-      Authenticating with LinkedIn...
+    <div className="Auth" style={fullPageStyle}>
+      {!error ? (
+        <div>Authenticating with LinkedIn...</div>
+      ) : (
+        <div>
+          Sorry, I couldn't authenticate with LinkedIn. 😔
+          <br />
+          <div>
+            <Link to="/">Return home?</Link>
+          </div>
+        </div>
+      )}
+
+      {userData && <Redirect to="/user" />}
+    </div>
+  );
+}
+
+function User() {
+  return (
+    <div className="User" style={fullPageStyle}>
+      User!
     </div>
   );
 }
@@ -51,6 +131,7 @@ function Routes() {
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/auth" component={Auth} />
+      <Route path="/user" component={User} />
     </Switch>
   );
 }
--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const BUNDLE = path.resolve(__dirname, "../client/build", "index.html");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
+const got = require("got");
+const FormData = require('form-data');
 const passportService = require("./services/passport");
 const passport = require("passport");
 
@@ -18,13 +20,55 @@ const app = express();
 
 app.use(logger("combined"));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // set public dir for app
 app.use("/", express.static(path.join(__dirname, "../client", "build")));
-app.get("/auth/linkedin/cb", (req, res) => {
-    res.status(200).send({ message: "success" });
+
+app.post("/auth/linkedin", async (req, res) => {
+  const { body } = req;
+  const SECRET = "Arnaldo was here";
+  const form = new FormData();
+  const formBody = {
+    grant_type: "authorization_code",
+    code:
+      body.authorizationCode,
+    redirect_uri: "http://localhost:3000/auth",
+    client_id: "86n2guu7lpgeen",
+    client_secret: SECRET
+  };
+
+  for (const key in formBody) {
+    form.append(key, formBody[key]);
+  }
+
+  try {
+    const reqAccessToken = await got.post(
+      "https://www.linkedin.com/oauth/v2/accessToken",
+      { body: form }
+    );
+    
+    let accessToken;
+
+    console.log("LinkedIn auth response:", reqAccessToken);
+
+    if (!getToken.body) {
+      throw new Error();
+    } else {
+      accessToken = reqAccessToken.body;
+    }
+
+    return res.status(200).send({
+      message: "success",
+      accessToken: accessToken.access_token,
+      expiresIn: accessToken.expires_in
+    });
+  } catch (error) {
+    return res.status(error.statusCode).send({
+      message: error.body
+    });
+  }
 });
 
 // routes
