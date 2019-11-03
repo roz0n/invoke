7a8c717d41041bc0805cfe16617ea4eaa97298bf
Arnold Rozon <arnold@rozon.org>
2019-11-02 19:25:22 -0500

    Added routes:

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+const authEndpoint = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=86n2guu7lpgeen&redirect_uri=http://localhost:3000/auth&state=fooobar&scope=r_liteprofile%20r_emailaddress%20w_member_social`;
 
 function App() {
   return (
+    <Router>
+      <Routes />
+    </Router>
+  );
+}
+
+function Home() {
+  return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Invoke (α)</p>
+        <small>Enter your LinkedIn credentials below to get started</small>
       </header>
+
+      <section>
+        <a href={authEndpoint}>
+          <button>Sign in with LinkedIn</button>
+        </a>
+      </section>
     </div>
   );
 }
 
+function Auth() {
+  const style = {
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    justifyContent: "space-around",
+    height: "100vh",
+    width: "100%"
+  };
+
+  return (
+    <div className="Auth" style={style}>
+      Authenticating with LinkedIn...
+    </div>
+  );
+}
+
+function Routes() {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route path="/auth" component={Auth} />
+    </Switch>
+  );
+}
+
 export default App;
+
+// http://localhost:3000/auth/linkedin/cb?code=AQShMMEdfWNPAJs-_jaKXAri9vfV9CNjdDr6fk7zO9Q_5_gi5AcbawT-Z0aZHlkXJJjXfMNJYAiFEprQrOSZiChodFLpoX-mNn5X4AFeefTQ0noCBSZdprumuqMTYRwEO2c_X7hU8-1U4td0iPKZXLwywvZomYDbbDXSspd1S2trlettqmAN-XZDh5g7-Q&state=fooobar
--- server/app.js
+++ server/app.js
@@ -1,25 +1,38 @@
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+const express = require("express");
+const path = require("path");
 const BUNDLE = path.resolve(__dirname, "../client/build", "index.html");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const passportService = require("./services/passport");
+const passport = require("passport");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const MONGO_URI = "mongodb://localhost:27017/invoke-alpha";
+
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+
+// const apiRoutes = require("./routes/api");
+// const authRoutes = require("./routes/auth");
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("combined"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // set public dir for app
 app.use("/", express.static(path.join(__dirname, "../client", "build")));
+app.get("/auth/linkedin/cb", (req, res) => {
+    res.status(200).send({ message: "success" });
+});
 
 // routes
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+
+// app.use("/api", apiRoutes);
+// app.use("/", authRoutes);
 
 // serve app
 app.get("*", (req, res) => res.sendFile(BUNDLE));
